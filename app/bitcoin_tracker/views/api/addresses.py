47ca18959459da98355790be1a7a963d8a706092
Evan Henri <ehenri@arbor.net>
2021-01-13 23:37:48 -0500

    Remove cached information when user deletes address

--- app/bitcoin_tracker/views/api/addresses.py
+++ app/bitcoin_tracker/views/api/addresses.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from rest_framework.viewsets import ModelViewSet
 
 from ...models import Address
@@ -7,3 +8,11 @@ from ...serializers import AddressSerializer
 class AddressesViewSet(ModelViewSet):
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        address = Address.objects.get(pk=kwargs['pk'])
+        settings.REDIS.delete(
+            f'{address}_balance',
+            f'{address}_transactions',
+        )
+        return super().destroy(request, *args, **kwargs)
